d0dbe3f8645a6cea79f7597c17a63f8e94f60179
Anurag-git04 <shawanurag18@gmail.com>
2024-02-14 23:52:50 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import logo from '../assets/Logo.svg'
+import {Link} from 'react-router-dom'
+import toast from 'react-hot-toast'
+
+const Navbar = (props) => {
+  let isLoggedIn = props.isLoggedIn
+  let setIsLoggedIn = props.setIsLoggedIn
+  return (
+    <div className='flex justify-evenly'>
+
+        <Link to="/">
+            <img src={logo} alt='Logo' width={160} height={32} loading='lazy' />
+        </Link>
+
+        <nav>
+        <ul className='flex gap-3'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Contract</Link>
+          </li>
+          
+        </ul>
+        </nav>
+       
+
+        {/* Login-Singup-Logout-DashBoard */}
+
+        <div className='flex ml-5 mr-3 gap-3'>
+          { !isLoggedIn &&
+            <Link to='/login'>
+              <button onClick={()=>{
+                setIsLoggedIn(true)
+                toast.success("Logged In")
+              }}>
+                Login
+              </button>
+            </Link>
+          }
+          { !isLoggedIn &&
+            <Link to='/signup'>
+              <button onClick={()=>{
+                setIsLoggedIn(true)
+                toast.success("Logged In")
+              }}>
+                SignUp
+              </button>
+            </Link>
+          }
+          { isLoggedIn &&
+            <Link to='/'>
+              <button onClick={()=>{
+                setIsLoggedIn(false)
+                toast.success("Logged Out")
+              }}>
+                Logout
+              </button>
+            </Link>
+          }
+          { isLoggedIn &&
+            <Link to='/dashboard'>
+              <button onClick={()=>{
+                setIsLoggedIn(false)
+                toast.success("Logged Out")
+              }}>
+                Dashboard
+              </button>
+            </Link>
+          }
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
